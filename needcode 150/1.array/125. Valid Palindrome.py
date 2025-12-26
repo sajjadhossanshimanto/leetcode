c41c587e82166973e1701316146db0b25eaf4f02
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-12-26 15:01:18 +0600

    2 pointer palindrome

--- needcode 150/1.array/125. Valid Palindrome.py
+++ needcode 150/1.array/125. Valid Palindrome.py
@@ -4,14 +4,18 @@ alpha numeric includes -> alphabers and number.
 
 '''
 
-def filter_alap(s):
+def filter_alap(s: str):
+    s = s.lower()
+    l = []
     for i in s:
         if (ord(i)>=97 and ord(i)<=122) or (48<=ord(i)<=57):
-            yield i
+            l.append(i)
+    
+    return "".join(l)
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(list(filter_alap(s.lower())))
+        s = filter_alap(s.lower())
         return s==s[::-1]
         return s
 
@@ -23,4 +27,18 @@ s.isPalindrome("0p")
 # %%
 ord("9")
 # %%
-# TODO: solve in O(logn) with 2 pointer
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = filter_alap(s)
+        # we could have applied that same fillter while iteration for palindrom match
+        
+
+        l, r = 0, len(s)-1
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+
+            l+=1
+            r-=1
+        
+        return True
